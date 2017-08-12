Megatron007/packages_apps_Settings

Allow PIE Control to be launched externally
For the PIE Tile basically...

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -136,6 +136,7 @@ import com.mrapocalypse.screwdshop.frags.FlingSettings;
 import com.mrapocalypse.screwdshop.frags.NavbarSettings;
 import com.mrapocalypse.screwdshop.frags.SmartbarSettings;
 import com.mrapocalypse.screwdshop.frags.PulseSettings;
+import com.mrapocalypse.screwdshop.frags.PieControl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -363,7 +364,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            PieControl.class.getName(),
     };
 
 

pixeldustproject/android_packages_apps_Settings

Signed-off-by: spezi77 <spezi77@gmx.com>

src/com/android/settings/SettingsActivity.java
@@ -140,6 +140,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.pixeldust.settings.PixelDustSettings;
+import com.pixeldust.settings.fragments.PieControl;
 
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
@@ -370,7 +371,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            PixelDustSettings.class.getName()
+            PixelDustSettings.class.getName(),
+            PieControl.class.getName(),
     };
 
 